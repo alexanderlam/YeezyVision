072edf567e31669ec2d784025b5c1fc0a0d94621
Alex Lam <alexlam1994@gmail.com>
2016-02-21 01:22:27 -0500

    DOOONE BITCHES

--- wordconvert/content.js
+++ wordconvert/content.js
@@ -1,4 +1,4 @@
-var pElements = document.querySelectorAll('p','tr','th','li','a');
+var pElements = document.querySelectorAll('p', 'tr', 'th', 'li', 'a');
 
 asyncLoop(pElements.length, function (loop) {
 
@@ -37,7 +37,7 @@ asyncLoop(pElements.length, function (loop) {
     }
 );
 
-var hElements = document.querySelectorAll('h1','h2','h3','h4','h5','h6');
+var hElements = document.querySelectorAll('h1', 'h2', 'h3', 'h4', 'h5', 'h6');
 
 asyncLoop(hElements.length, function (loop) {
 
@@ -75,3 +75,48 @@ asyncLoop(hElements.length, function (loop) {
         console.log('cycle ended')
     }
 );
+
+
+$('img').each(
+    function () {
+        $(this).css({
+            'display': 'block',
+            'position': 'absolute',
+            'top': '0',
+            'left': '0'
+        });
+        var imgWidth = $(this).width();
+        var imgHeight = $(this).height();
+        if (imgWidth >= 150) {
+            var wrapperDiv = "<div style='display: block; position: relative; height: " + imgHeight + "px; width: " + imgWidth + "px;'></div>";
+            $(this).wrap(wrapperDiv);
+            var curImg = this;
+            getFaceLocation($(this)[0].src, function (jsonResponse) {
+                var responseArray = $.map(jsonResponse, function(el) { return el });                    
+                console.log("mapSuccess: "+responseArray);
+                    asyncLoop(responseArray.length, function (loop3) {
+                        console.log("loopSuccess");
+                            var singleResponse = responseArray[loop3.iteration()];
+                            var headTop = singleResponse['faceRectangle']['top'];
+                            var headLeft = singleResponse['faceRectangle']['left'];
+                            var headHeight = singleResponse['faceRectangle']['height'];
+                            var headWidth = singleResponse['faceRectangle']['width'];
+                            var min = 1;
+                            var max = 11;
+                            var randImg = "http://alexanderlam.ca/images/sqr_kanye-" + (Math.floor(Math.random() * (max - min + 1)) + min) + ".png";
+                            
+                            var yeezyHead = "<img style='display: block; position: absolute; top: " + headTop+"px; left: " +
+                                headLeft+"px; height:" + headHeight + "px; width:" + headWidth + "px; z-index: 1;' alt='' src='"+randImg+"'>";
+                        console.log(yeezyHead);
+                            $(curImg).before(yeezyHead,loop3.next());
+                            
+                        },
+                        function () {
+                            console.log('cycle ended');
+                        }
+
+                    );
+            });
+        }
+
+    });
